54502ef2c6ffd45e24f2ed26c3139703ed9a157a
MeJ <don.juncos@gmail.com>
2021-08-28 22:25:46 -0300

    fix Cors in empleadoController

--- src/main/java/com/hotel/controller/EmpleadoRestController.java
+++ src/main/java/com/hotel/controller/EmpleadoRestController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:4200"}, maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class EmpleadoRestController {
